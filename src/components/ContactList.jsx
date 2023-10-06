ce2ea42022d75c9c21a41ff966fc014202b3f11d
Tom Tarpey <tomtarpeydev@gmail.com>
2023-10-06 01:41:15 +0100

    added the structure for the contactlist.jsx file and some static data to test with

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
 function ContactList() {
-  return (
-    <div>ContactList</div>
-  )
+    const [contacts, setContacts] = useState([
+        {id: 1, name: "Dave Jones", email: "dave@jones.com"},
+        {id: 2, name: "John Jenkins", email: "john@jenkins.com"},
+        {id: 3, name: "Jim Smith", email: "jim@smith.com"},
+        {id: 4, name: "Bob Doe", email: "bob@doe.com"}
+    ]);
+
+    return (
+        <div>
+            <h2>Contacts</h2>
+            <ul>
+            {contacts.map((contact) => (
+                <li key={contact.id}>
+                    {contact.name} - {contact.email}
+                    <Link to={`/edit/${contact.id}`}>Edit</Link>
+                </li>
+            ))}
+            </ul>
+        </div>
+    )
 }
 
 export default ContactList
