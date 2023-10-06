627fd880c7ec7b48852e6c59a32b364030ba31ae
Tom Tarpey <tomtarpeydev@gmail.com>
2023-10-06 17:29:42 +0100

    added some boilerplate for the AddContact.jsx form

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,7 +1,25 @@
 function AddContact() {
-  return (
-    <div>AddContact</div>
-  )
+
+    
+    // TODO: create a handleAddContact function here
+    
+    return (
+        <div>
+            <h2>Add Contact</h2>
+            <form>
+                <div>
+                    <label>Name:</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input type="text" />
+                </div>
+                <button type="button">Add Contact</button>
+            </form>
+        </div>
+
+    )
 }
 
 export default AddContact
