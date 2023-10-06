7ef44de1171f445ae606be18bc133de436ca01fe
hakikat011 <114380967+hakikat011@users.noreply.github.com>
2023-10-05 19:28:09 -0700

    Update ContactList.jsx
    tried to solve the mentioned issue on the board

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,33 +1,36 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-
 function ContactList() {
-    const [contacts, setContacts] = useState([
-        {id: 1, name: "Dave Jones", email: "dave@jones.com"},
-        {id: 2, name: "John Jenkins", email: "john@jenkins.com"},
-        {id: 3, name: "Jim Smith", email: "jim@smith.com"},
-        {id: 4, name: "Bob Doe", email: "bob@doe.com"}
-    ]);
+    const [contacts, setContacts] = useState([]);
 
     useEffect(() => {
-        console.log("Running the use effect...")
-        console.log('TODO: fetch data from the backend and use the setContacts to set the state')
+        console.log("Running the use effect...");
+        
+        // Make a GET request to fetch contacts from the backend
+        fetch('http://localhost:3000/contacts')
+            .then(response => response.json())
+            .then(data => {
+                setContacts(data); // Update state with fetched data
+            })
+            .catch(error => console.error('Error fetching data:', error));
     }, []);
 
     return (
         <div>
             <h2>Contacts</h2>
             <ul>
-            {contacts.map((contact) => (
-                <li key={contact.id}>
-                    {contact.name} - {contact.email}
-                    <Link to={`/edit/${contact.id}`}>Edit</Link>
-                </li>
-            ))}
+                {contacts.map((contact) => (
+                    <li key={contact.id}>
+                        {contact.name} - {contact.email}
+                        <Link to={`/edit/${contact.id}`}>Edit</Link>
+                    </li>
+                ))}
             </ul>
         </div>
     )
 }
 
 export default ContactList
+
+
