014c800cb00e126a935b16f43e8196f6af543af5
Tom Tarpey <tomtarpeydev@gmail.com>
2023-10-07 23:46:49 +0100

    added boilerplate and todos for EditContact.jsx

--- src/components/EditContact.jsx
+++ src/components/EditContact.jsx
@@ -1,7 +1,17 @@
+// will need to import useEffect, useState, useNavigate and useParams
+
 function EditContact() {
+    // get the id from the params
+    // TODO: create a useEffect to pull the contact by id
+
+    // TODO: Create a handleEditContact and add it to the form
+
   return (
-    <div>EditContact</div>
-  )
+    <div>
+      <h2>Edit Contact</h2>
+      {/* conditionally render a form only when the contact is loaded from a useEffect */}
+    </div>
+  );
 }
 
-export default EditContact
+export default EditContact;
